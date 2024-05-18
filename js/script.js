bc5b230a0b9e8bf7b1ba7e7e6abd3dd9d5e9127b
unknown <teteulima@outlook.com>
2024-05-18 00:03:16 -0300

    adicionei o slide dos participantes - Thony

--- js/script.js
+++ js/script.js
@@ -50,3 +50,35 @@ function navBar() {
     nav.classList.add('open')
   }
 }
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  // slidesPerGroup: 3,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  // loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0:{
+          slidesPerView: 1,
+      },
+      670: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
